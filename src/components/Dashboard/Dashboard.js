76cd83e577ca17e53dc5ab2b2142351bb8d3f6c7
Ayushi <ayushigupta283203@gmail.com>
2025-01-15 14:18:38 +0530

    use prettier

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,55 +1,65 @@
-import React from 'react'
-import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
-import TableRowsOutlinedIcon from '@mui/icons-material/TableRowsOutlined';
-import ViewWeekOutlinedIcon from '@mui/icons-material/ViewWeekOutlined';
-import FilterAltOutlinedIcon from '@mui/icons-material/FilterAltOutlined';
-import SwapVertOutlinedIcon from '@mui/icons-material/SwapVertOutlined';
-import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
-import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
-import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
-import Enrich from '../../images/enrich.svg'
-import './dashboard.css';
-import Table from '../Table/Table';
+import React from "react";
+import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
+import TableRowsOutlinedIcon from "@mui/icons-material/TableRowsOutlined";
+import ViewWeekOutlinedIcon from "@mui/icons-material/ViewWeekOutlined";
+import FilterAltOutlinedIcon from "@mui/icons-material/FilterAltOutlined";
+import SwapVertOutlinedIcon from "@mui/icons-material/SwapVertOutlined";
+import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
+import FileDownloadOutlinedIcon from "@mui/icons-material/FileDownloadOutlined";
+import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
+import Enrich from "../../images/enrich.svg";
+import "./dashboard.css";
+import Table from "../Table/Table";
 
 const Dashboard = () => {
   return (
     <>
-    <div className='dashboard-container'>
-      <div className='dashboard-left'>
-        <div className='dashboard-search-input'>
-        <SearchRoundedIcon style={{backgroundColor:"transparent"}}/>
-        <input type='text' placeholder='Search'/>
+      <div className="dashboard-container">
+        <div className="dashboard-left">
+          <div className="dashboard-search-input">
+            <SearchRoundedIcon style={{ backgroundColor: "transparent" }} />
+            <input type="text" placeholder="Search" />
+          </div>
+          <div className="dashboard-smaller-container">
+            <TableRowsOutlinedIcon style={{ width: "22px", height: "22px" }} />
+            <span>1/1 Row</span>
+          </div>
+          <div className="dashboard-smaller-container">
+            <ViewWeekOutlinedIcon style={{ width: "22px", height: "22px" }} />
+            <span>3/3 Column</span>
+          </div>
+          <div className="dashboard-smaller-container">
+            <FilterAltOutlinedIcon style={{ width: "22px", height: "22px" }} />
+            <span>0 filter</span>
+          </div>
+          <div className="dashboard-smaller-container">
+            <SwapVertOutlinedIcon style={{ width: "22px", height: "22px" }} />
+            <span>Sort</span>
+          </div>
         </div>
-        <div className='dashboard-smaller-container'>
-        <TableRowsOutlinedIcon style={{width:"22px", height:"22px"}}/>
-        <span>1/1 Row</span>
-        </div>
-        <div className='dashboard-smaller-container'>
-        <ViewWeekOutlinedIcon style={{width:"22px", height:"22px"}}/>
-        <span>3/3 Column</span>
-        </div>
-        <div className='dashboard-smaller-container'>
-        <FilterAltOutlinedIcon style={{width:"22px", height:"22px"}}/>
-        <span>0 filter</span>
-        </div>
-        <div className='dashboard-smaller-container'>
-        <SwapVertOutlinedIcon style={{width:"22px", height:"22px"}}/>
-        <span>Sort</span>
+        <div className="dashboard-right">
+          <button className="dashboard-enrich">
+            <img
+              style={{
+                width: "16px",
+                height: "16px",
+                backgroundColor: "transparent",
+              }}
+              src={Enrich}
+              alt=""
+            />
+            Enrich
+          </button>
+          <ShareOutlinedIcon style={{ width: "22px", height: "22px" }} />
+          <FileDownloadOutlinedIcon style={{ width: "22px", height: "22px" }} />
+          <DeleteOutlineOutlinedIcon
+            style={{ width: "22px", height: "22px" }}
+          />
         </div>
       </div>
-      <div className='dashboard-right'>
-        <button className='dashboard-enrich'>
-          <img style={{width:"16px",height:"16px",backgroundColor:"transparent"}} src={Enrich} alt=''/>
-          Enrich
-        </button>
-       <ShareOutlinedIcon style={{width:"22px", height:"22px"}}/>
-       <FileDownloadOutlinedIcon style={{width:"22px", height:"22px"}}/>
-       <DeleteOutlineOutlinedIcon style={{width:"22px", height:"22px"}}/>
-      </div>
-    </div>
-    <Table/>
+      <Table />
     </>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,29 +1,36 @@
-import React from 'react'
-import './header.css'
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import ToggleOnRoundedIcon from '@mui/icons-material/ToggleOnRounded';
-import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import React from "react";
+import "./header.css";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ToggleOnRoundedIcon from "@mui/icons-material/ToggleOnRounded";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 
 const Header = () => {
   return (
-    <div className='header-container'>
-      <div className='header-container-right'>
-      <ArrowBackIcon style={{width:"18px", height:"18px"}}/>
-      <span>Name of the file</span>
-    </div>
-    <div className='header-container-left'>
-      <div className='header-subcontainer-left'>
-      <ToggleOnRoundedIcon style={{width:"40px",height:"40px",color:"#0E9F6E"}} />
-      <span>Auto save</span>
+    <div className="header-container">
+      <div className="header-container-right">
+        <ArrowBackIcon style={{ width: "18px", height: "18px" }} />
+        <span>Name of the file</span>
       </div>
-      <div className='header-circle'>
-      <PersonOutlinedIcon style={{width:"18px", height:"18px",backgroundColor:"transparent",color:"#D03801"}}/>
+      <div className="header-container-left">
+        <div className="header-subcontainer-left">
+          <ToggleOnRoundedIcon
+            style={{ width: "40px", height: "40px", color: "#0E9F6E" }}
+          />
+          <span>Auto save</span>
+        </div>
+        <div className="header-circle">
+          <PersonOutlinedIcon
+            style={{
+              width: "18px",
+              height: "18px",
+              backgroundColor: "transparent",
+              color: "#D03801",
+            }}
+          />
+        </div>
       </div>
-      
     </div>
-    </div>
-    
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
